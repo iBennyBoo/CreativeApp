7de326f52f14e5169ed96826393615c5328ad27d
Ben Berman <bpb062004@gmail.com>
2021-09-07 07:15:28 -0500

    Final product of the Pythagorean Theorem Creative App

--- CreativeAppMod1/ViewController.swift
+++ CreativeAppMod1/ViewController.swift
@@ -13,7 +13,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var FirstNumberTextField: UITextField!
     @IBOutlet weak var SecondNumberTextField: UITextField!
     @IBOutlet weak var InfoLabel: UILabel!
+    @IBOutlet weak var AnswerLabel: UILabel!
     @IBOutlet weak var ButtonOutlet: UIButton!
+    @IBOutlet weak var SwitchEquationButton: UIButton!
     @IBOutlet weak var LoadingOutlet: UIActivityIndicatorView!
     var x = 0
     override func viewDidLoad() {
@@ -30,12 +32,37 @@ class ViewController: UIViewController, UITextFieldDelegate {
         ButtonOutlet.layer.borderColor = black.cgColor
         ButtonOutlet.layer.borderWidth = 1
         ButtonOutlet.layer.cornerRadius = 40
+        SwitchEquationButton.layer.borderColor = black.cgColor
+        SwitchEquationButton.layer.borderWidth = 1
+        SwitchEquationButton.layer.cornerRadius = 40
+        AnswerLabel.layer.borderColor = black.cgColor
+        AnswerLabel.layer.borderWidth = 1
         LoadingOutlet.hidesWhenStopped = true
         self.LoadingOutlet.stopAnimating()
+        AnswerLabel.isHidden = true
+        
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func SwitchEquationButton(_ sender: Any) {
+        if(x == 0){
+            AnswerLabel.isHidden = true
+        FirstNumberLabel.text = "A or B Value"
+        SecondNumberLabel.text = "C Value"
+        InfoLabel.text = "Welcome to the Pythagorean Theorem Calculator! Enter your A/B and C values in the Text Fields."
+            x = 1
+        } else if (x == 1){
+            FirstNumberLabel.text = "A Value"
+            SecondNumberLabel.text = "B Value"
+            InfoLabel.text = "Welcome to the Pythagorean Theorem Calculator! Enter your A and B values in the Text Fields."
+            x = 0
+        }
+    }
+    
+    
     @IBAction func ButtonOutlet(_ sender: Any) {
+        AnswerLabel.isHidden = true
+        InfoLabel.text = ""
         let numberOne = FirstNumberTextField.text!
         let numberTwo = SecondNumberTextField.text!
         Timer.scheduledTimer(withTimeInterval: 0.0, repeats: false) { [self] (t) in self.LoadingOutlet.startAnimating()
@@ -44,11 +71,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
         Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { [self] (t) in self.LoadingOutlet.stopAnimating()
             if let condition =  Double(numberOne){
                 if let condition = Double(numberTwo){
-                    let stepOne = Double(numberOne)! * Double(numberOne)!
+                    if(x==0){
+                        let stepOne = Double(numberOne)! * Double(numberOne)!
                     let stepTwo = Double(numberTwo)! * Double(numberTwo)!
                     let stepThree = stepOne + stepTwo
-                    let answer = stepThree.squareRoot()
-                    InfoLabel.text = "Your C value is equal to \(answer). Enter your next set of A and B values in the Text Fields."
+                    let stepFour = stepThree.squareRoot()
+                    let answer = round(stepFour * 1000.0)/1000.0
+                        AnswerLabel.isHidden = false
+                    AnswerLabel.text = "\(answer)"
+                    InfoLabel.text = "Enter your next set of values in the Text Fields."
+                    } else if (x == 1){
+                        let stepOne = Double(numberOne)! * Double(numberOne)!
+                        let stepTwo = Double(numberTwo)! * Double(numberTwo)!
+                        let stepThree = -(stepTwo - stepOne)
+                        print(stepThree)
+                        let stepFour = stepThree.squareRoot()
+                        let answer = round(stepFour * 1000.0)/1000.0
+                        
+                        if(stepTwo >= stepOne){
+                            AnswerLabel.isHidden = false
+                            AnswerLabel.text = "Error!"
+                        } else{
+                            AnswerLabel.isHidden = false
+                            AnswerLabel.text = "\(answer)"
+                            print(answer)
+                            InfoLabel.text = "Enter your next set of values in the Text Fields."
+                        }
+                    }
                 }else{
                     InfoLabel.text = "One or both of your values is not a number, please try again"
                 }
@@ -70,4 +119,3 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
 }
-
